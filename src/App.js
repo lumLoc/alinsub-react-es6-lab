12a3a9c505f5464fdbb886cd0df42a2fae6df540
Jarren S. Gatdula <Luminance.10000@gmail.com>
2026-01-22 19:09:19 +0800

    Fetch and display users from API

--- src/App.js
+++ src/App.js
@@ -1,17 +1,33 @@
 import React from 'react';
 import UserList from './UserList';
 import Counter from './Counter';
+import UserAPI from './UserAPI';
 
 function App() {
+  const isLoggedIn = true;
   const users = ['Alice', 'Bob', 'Charlie'];
 
   return (
     <div>
+      <h1>Karl Alinsub's First Website</h1>
+
+      {/* Conditional Rendering */}
+      {isLoggedIn ? <p>Welcome back!</p> : <p>Please log in</p>}
+
+      {/* Local user list */}
       <UserList users={users} />
+
+      {/* Counter example */}
       <Counter />
+
+      {/* API fetched users */}
+      <h2>Users from API</h2>
+      <UserAPI />
     </div>
   );
 }
 
 export default App;
+
+
 
---
+++ src/UserAPI.js
@@ -0,0 +1,26 @@
+import React, { useEffect, useState } from 'react';
+
+function UserAPI() {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    async function fetchUsers() {
+      const res = await fetch(
+        'https://jsonplaceholder.typicode.com/users'
+      );
+      const data = await res.json();
+      setUsers(data);
+    }
+    fetchUsers();
+  }, []);
+
+  return (
+    <ul>
+      {users.map(user => (
+        <li key={user.id}>{user.name}</li>
+      ))}
+    </ul>
+  );
+}
+
+export default UserAPI;
